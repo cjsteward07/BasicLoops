d26b76f1228dc8b9285c854ccc643e63014d4f63
unknown <cjsteward07@gmail.com>
2022-10-03 22:13:29 -0400

    init

---
+++ BasicLoops/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("-Basic Loops Lab-\n");
+
+//string userContinue = string.Empty;
+//do
+//{
+//    Console.WriteLine("Hello, World");
+//    Console.WriteLine("Would you like to continue [y/n]?");
+//    userContinue = Console.ReadLine().ToLower().Trim();
+//}
+//while (userContinue != "n");
+
+Console.WriteLine("Please enter a number: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < userNumber; i ++)
+{
+    userNumber--;
+    Console.WriteLine(userNumber);
+}
